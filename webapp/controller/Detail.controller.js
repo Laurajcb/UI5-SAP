c9f9820664f67834a1bfbb0e46c454410850312a
Laura Callejas <callejaslaura96@gmail.com>
2025-03-18 02:25:21 +0000

    [ADD] detail, viw, controller and its logic

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "project/practice/utils/HomeHelper"
+], function(Controller, HomeHelper) {
+	"use strict";
+
+	return Controller.extend("project.practice.controller.Detail", {
+        onInit: function() {
+            
+        }
+	});
+});
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -7,10 +7,21 @@ sap.ui.define([
     return Controller.extend("project.practice.controller.Home", {
         onInit() {
             console.log('Init Home')
+            this.oRouter = this.getOwnerComponent().getRouter()
         },
         onPress: async function(){
             let oDatos = await HomeHelper.getDataProducts(); 
             await HomeHelper.setProductModel(this, oDatos[0].results);
+        },
+        onItemPress: function(oEvent) {
+            console.log("item press")
+            let oSource = oEvent.getSource()
+            let aDatos = oSource.getBindingContext("ProductCollection").getObject()
+
+            this.oRouter.navTo("detail", {
+                ProductID: aDatos.ProductID
+            })
         }
+
     });
 });
